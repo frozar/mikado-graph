d6a0459cf1251099877cca5c387ee084671044c3
frozar <fabien.rozar@gmail.com>
2020-05-11 17:25:23 +0200

    Ready to handle camera panning with a queue event in the camera.cljs side.

--- src/main/bubble/pan.cljs
+++ src/main/bubble/pan.cljs
@@ -8,67 +8,50 @@
    [goog.events EventType]
    ))
 
-(defn- current-time []
-  (.now js/Date))
+(defn apply-pan [mouse-pos-svg-px init-mouse-pos-svg-px init-cam-pos-svg-user]
+  (let [new-camera-center
+        (->> mouse-pos-svg-px
+             (#(map - % init-mouse-pos-svg-px))
+             (map #(camera/scale-dist %))
+             (#(map - init-cam-pos-svg-user %))
+             (zipmap [:cx :cy]))
 
-(defn- delta-time [starting-time current-time]
-  (- current-time starting-time))
+        new-camera
+        (camera/update-camera new-camera-center)
+        ]
+    (camera/set-camera! new-camera)
+    ))
+
+(def init-cam-pos-svg-user (atom nil))
+(def init-mouse-pos-svg-px (atom nil))
 
 (defn pan-move-fn []
-  (let [{init-cam-cx-svg-user :cx init-cam-cy-svg-user :cy} (camera/state)
-        init-cam-pos-svg-user [init-cam-cx-svg-user init-cam-cy-svg-user]
-        init-mouse-pos-svg-px (atom nil)
-        mouse-position (atom [])
-        starting-time (atom nil)]
+  (let [{init-cam-cx-svg-user :cx init-cam-cy-svg-user :cy} (camera/state)]
     (fn [evt]
       (let [mouse-pos-svg-px
             (coord/win-px->svg-px [(.-clientX evt) (.-clientY evt)])]
-        (when (nil? @init-mouse-pos-svg-px)
+        (when (or (nil? @init-cam-pos-svg-user)
+                  (nil? @init-mouse-pos-svg-px))
+          (reset! init-cam-pos-svg-user [init-cam-cx-svg-user init-cam-cy-svg-user])
           (reset! init-mouse-pos-svg-px mouse-pos-svg-px))
 
-        (if (nil? @starting-time)
-          (do
-            (reset! starting-time (current-time))
-            (swap! mouse-position conj [mouse-pos-svg-px 0]))
-          (let [[_ previous-spent-time] (last @mouse-position)
-                spent-time (delta-time @starting-time (current-time))]
-            ;; 500: 500 millisecond
-            (when (< 500 (delta-time previous-spent-time spent-time))
-              (swap! mouse-position conj [mouse-pos-svg-px spent-time])))
-          )
-
-        ;; (prn [mouse-x-svg-px mouse-y-svg-px])
-        ;; (prn "mouse-position" @mouse-position)
-
-        (let [new-camera-center
-              (->> mouse-pos-svg-px
-                   (#(map - % @init-mouse-pos-svg-px))
-                   (map #(camera/scale-dist %))
-                   (#(map - init-cam-pos-svg-user %))
-                   (zipmap [:cx :cy]))
-              ;; _ (prn "new-camera-center" new-camera-center)
-
-              new-camera
-              (camera/update-camera new-camera-center)
-              ]
-          (camera/set-camera! new-camera)
-          ;; (camera/animate-camera-transition new-camera 2) ;; ca fout la merde
-          )
+        (apply-pan mouse-pos-svg-px @init-mouse-pos-svg-px @init-cam-pos-svg-user)
         ))))
 
 (defn pan-end-fn [pan-move pan-end-atom on-end]
   (fn []
     (events/unlisten js/window EventType.MOUSEMOVE pan-move)
     (events/unlisten js/window EventType.MOUSEUP @pan-end-atom)
-    (on-end)))
+    (on-end)
+    (reset! init-cam-pos-svg-user nil)
+    (reset! init-mouse-pos-svg-px nil)
+    ))
 
 (defn should-center
   "If the graph is not more visible, call the home event to 'center'
   the view around the graph."
   []
   ;; 10e-3: an arbitrary small value
-  ;; (prn "now" (.now js/Date))
-  ;; (prn "(not (camera/in-pan-limit? 10e-3)" (not (camera/in-pan-limit? 10e-3)))
   ;; TODO: adjust with the animation
   (when (not (camera/in-pan-limit? 10e-3))
     (camera/home-evt)))
