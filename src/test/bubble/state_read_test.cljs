8a89bc638df227bf03715a7a381601c58b0b21ce
frozar <fabien.rozar@gmail.com>
2020-06-14 12:04:12 +0200

    Add tests for get-bubbles and get-links

--- src/test/bubble/state_read_test.cljs
+++ src/test/bubble/state_read_test.cljs
@@ -11,6 +11,50 @@
   (#'sd/init-appstate)
   )
 
+(deftest get-bubbles_basic
+  (is
+   (=
+    (-> appstate-1-bubble
+        sr/get-bubbles
+        keys)
+    (list "root")))
+  (is
+   (=
+    (-> appstate-1-bubble
+        (sw/create-bubble-and-link ROOT-BUBBLE-ID 450 450 "bubble-1")
+        sr/get-bubbles
+        keys)
+    (list "root" "bubble-1")))
+  (is
+   (=
+    (-> appstate-1-bubble
+        (sw/create-bubble-and-link ROOT-BUBBLE-ID 450 450 "bubble-1")
+        (sw/create-bubble-and-link ROOT-BUBBLE-ID 450 450 "bubble-2")
+        sr/get-bubbles
+        keys)
+    (list "root" "bubble-1" "bubble-2"))))
+
+(deftest get-links_basic
+  (is
+   (=
+    (-> appstate-1-bubble
+        sr/get-links)
+    []))
+  (is
+   (=
+    (-> appstate-1-bubble
+        (sw/create-bubble-and-link ROOT-BUBBLE-ID 450 450 "bubble-1")
+        sr/get-links)
+    [{:src "root", :dst "bubble-1"}]))
+  (is
+   (=
+    (-> appstate-1-bubble
+        (sw/create-bubble-and-link ROOT-BUBBLE-ID 450 450 "bubble-1")
+        (sw/create-bubble-and-link ROOT-BUBBLE-ID 450 450 "bubble-2")
+        sr/get-links)
+    [{:src "root", :dst "bubble-1"}
+     {:src "root", :dst "bubble-2"}])))
+
 (deftest appstate->graph_basic
   (is
    (=
